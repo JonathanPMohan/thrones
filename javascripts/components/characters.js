6b1ab2b1ec3af81454cba43d9ffa141e631e7e96
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-10-01 21:14:38 -0500

    updated characters

--- javascripts/components/characters.js
+++ javascripts/components/characters.js
@@ -1,7 +1,7 @@
 import { printToDom } from "../helpers/util.js";
 
 const characters = [
-    {id: 'character1', name: 'John Snow', house: 'Stark', imageURL: "https://" },
+    {id: 'character1', name: 'John Snow', house: 'Stark', imageURL: '../images/snow.jpg' },
       {id: 'character2', name: 'Daenerys Targaryen', house: 'Targaryen', imageURL: "https://" },
       {id: 'character3', name: 'Cersei Lannister', house: 'Lannister', imageURL: "https://" },
       {id: 'character4', name: 'Tyrion Lannister', house: 'Lannister', imageURL: "https://" }
@@ -15,7 +15,14 @@ console.log('characters');
 const charactersBuilder = () => {
     let domString = '';
     characters.forEach((character) => {
-        domString += `<h1>${character.name}</h1>`
+        domString += `<div class='col-2 character">`
+        domString += `<div class="card">`;
+        domString += `<img class="card-img-top" src="${character.imageUrl}" alt="${character.name}">`;
+        domString += `<div class="card-body">`
+        domString +=    `<h5 class="card-title">${character.name}</h>`;
+        domString +=        `</div>`;
+        domString +=    `</div>`;
+        domString += `</div>`;
     });
     printToDom(domString);
 }
